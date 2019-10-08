166c95d22811fe51139e2688beb1c82956fc2645
Gabriele Trabucco <gabrynet83@gmail.com>
2019-10-09 00:42:48 +0200

    Simplify Replay Publisher

--- TestCombine.playground/Pages/Replay.xcplaygroundpage/Contents.swift
+++ TestCombine.playground/Pages/Replay.xcplaygroundpage/Contents.swift
@@ -5,97 +5,66 @@ import Combine
 
 // Replay
 
-public extension Publishers {
+extension Publishers {
 
-    final class Replay<Output, Failure: Error>: ConnectablePublisher {
+    public class Replay<Output, Failure: Error>: ConnectablePublisher {
 
-
-        // ***************** Do not use directly this variables ************************ /
-        // Workaround for Fucking issue with Property Wrappers.
-        // Seems they does not to accept a Generic Concrete Type as input
-        // Compiler crash with no error message.
-        @Synchronized private var _buffer: [Any] = []
-        @Synchronized private var _replay: Any = PassthroughSubject<Output, Failure>()
-        // ***************************************************************************** /
-
-        private var buffer: [Output] {
-            get { _buffer as! [Output] }
-            set { _buffer = newValue }
-        }
-        private var replay: PassthroughSubject<Output, Failure> { _replay as! PassthroughSubject<Output, Failure> }
-
-        @Synchronized private var bag: [AnyCancellable] = []
-
-        private let bufferSize: Int
-        private let upstream: MakeConnectable<AnyPublisher<Output, Failure>>
+        private let subject = CurrentValueSubject<[Output], Failure>([])
+        private let upstream: AnyPublisher<Output, Failure>
+        private let multicast: Publishers.Multicast<AnyPublisher<[Output], Failure>, CurrentValueSubject<[Output], Failure>>
 
         public init<P: Publisher>(buffer size: Int, upstream: P) where P.Output == Output, P.Failure == Failure {
-            self.bufferSize = Swift.max(0, size)
-            self.upstream = MakeConnectable(upstream: upstream.eraseToAnyPublisher())
-
-            self.upstream.sink(receiveCompletion: { _ in }, receiveValue: { [weak self] value in
-                guard let self = self else { return }
-                self.updateValues(with: value)
-            }).store(in: &bag)
-        }
-
-        private func updateValues(with value: Output) {
-            var newValues = buffer + [value]
-            if newValues.count > bufferSize {
-                let n = newValues.count - bufferSize
-                newValues = Array(newValues.dropFirst(n))
+            let shared = upstream.share().eraseToAnyPublisher()
+            self.upstream = shared
+            self.multicast = shared
+                .map { [$0] }
+                .scan([Output]()) {
+                    var result = $0 + $1
+                    if result.count > size {
+                        let toDrop = result.count - size
+                        result = Array(result.dropFirst(toDrop))
+                    }
+                    return result
             }
-            buffer = newValues
-            replay.send(value)
+            .eraseToAnyPublisher()
+            .multicast(subject: subject)
         }
 
         public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-            replay.receive(subscriber: subscriber)
-            buffer.forEach { _ = subscriber.receive($0) }
+            let buffer = subject.value
+                .publisher
+                .setFailureType(to: Failure.self)
+            upstream
+                .prepend(buffer)
+                .receive(subscriber: subscriber)
         }
 
-        @WeakRef private var connectCancellable: AnyCancellable?
-
         public func connect() -> Cancellable {
-            if let cancellable = connectCancellable {
-                return cancellable
-            } else {
-                let connection = upstream.connect()
-                let cancellable = AnyCancellable { connection.cancel() }
-                self.connectCancellable = cancellable
-                return cancellable
-            }
+            return multicast.connect()
         }
     }
 }
 
 extension Publisher {
 
-    func replay(buffer size: Int = 1) -> Publishers.Replay<Output, Failure> {
-        Publishers.Replay(buffer: size, upstream: eraseToAnyPublisher())
+    func replay2(buffer size: Int = 1) -> Publishers.Replay<Output, Failure> {
+        Publishers.Replay(buffer: size, upstream: self)
     }
 }
-let p = PassthroughSubject<Int, Never>()
-let r = p.replay(buffer: 5)
 
-let c1 = r.connect()
+let t = PassthroughSubject<Int, Never>()
+let r2 = t.replay2(buffer: 3).autoconnect()
 
-p.send(5)
-p.send(6)
-p.send(7)
-p.send(8)
-//p.send(completion: .finished)
-p.send(9)
-p.send(10)
-p.send(11)
+let cc = r2
+//    .print()
+    .sink { _ in }
 
-let c2 = r
-    .print()
-    .sink { print("1: \($0)") }
+t.send(0)
+t.send(1)
+t.send(2)
+t.send(3)
+t.send(completion: .finished)
 
-p.send(12)
-
-
-let c3 = r
-    .print()
-    .sink { print("2: \($0)") }
+let ccc = r2
+.print()
+.sink { _ in }
